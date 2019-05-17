396f91460324e0c8682fa23cb29551a8865abe32
Joao Borges <joao.roseira.borges@gmail.com>
2019-05-18 00:12:44 +0100

    -> New version using input parameter to feed the Excel file to be processed
    -> Minor correction in the Save method syntax, which cleaned the stdout

--- Splitter.ps1
+++ Splitter.ps1
@@ -5,24 +5,43 @@ For each one of the sheets after sheet "HLQ", create a new workbook composed by 
 	- current sheet 
 #>
 
-# set input Excel filename (also used as a base filename for the splited workbooks)
-$WorkbookName = "GC_DATA_LINEAGE_v1D.XLSX"
-$filepath ="C:\Users\0101675\Documents\BCP - CDO\Lineage - PS-GC\"+$WorkbookName
+# Get this Script Name 
+$ThisScript = (Get-Item $PSCommandPath ).Name
+
+# Check input parameter
+if ( $args.count -eq 0  ){
+   echo "================================================="
+   echo "Missing input parameter!"
+   echo " "
+   echo "  Usage: .\$ThisScript <Input File>"
+   echo "Example: .\$ThisScript GC_DATA_LINEAGE_v1D.XLSX"
+   echo "================================================="
+   echo " "
+   exit
+}
+
+# Set input Excel filename from the given input parameter ( which will also be used as a base filename for the splited workbooks)
+$WorkbookName=$args[0]
+$filepath=(Get-Item $WorkbookName)
+$filename=(Get-Item $WorkbookName).Name
 $WorkbookName = $filepath -replace ".xlsx", ""
 
-#####################################################
+### Init Excel Object
 $Excel = New-Object -ComObject "Excel.Application" 
 $Excel.Visible = $false #Runs Excel in the background. 
 $Excel.DisplayAlerts = $false #Supress alert messages. 
 $Workbook = $Excel.Workbooks.open($filepath)
-$numb_sheets = $Workbook.Worksheets.Count
+
+# set specific parameters
+$numb_sheets = $Workbook.Worksheets.Count-1
 $sheet_HLQ = $WorkBook.Worksheets(1)
-#
 $FileFormat = [Microsoft.Office.Interop.Excel.XlFileFormat]::xlOpenXMLWorkbook 
 $output_type = "xlsx"
 
 # Loop on sheets
-write-Output "Processing $WorkbookName with $numb_sheets sheets..." 
+write-Output "============================================================================================================= "
+write-Output "Processing Excel file $filename with $numb_sheets sheets containing Lineage Information Input ..." 
+write-Output ""
 $i=0
 foreach($Worksheet in $Workbook.Worksheets) {
     $i++
@@ -32,20 +51,20 @@ foreach($Worksheet in $Workbook.Worksheets) {
 		$Worksheet.copy()
         $newExcelFile = $WorkbookName + "_" + $Worksheet.Name + "." + $output_type 
         $Excel.ActiveWorkbook.SaveAs($newExcelFile, $FileFormat) 
-        $Excel.ActiveWorkbook.Close
+        $Excel.ActiveWorkbook.Close()
 
-        write-Output "Created file: $newExcelFile with sheet $Worksheet.Name"
+        write-Output "Created file: $newExcelFile"
 		
 		# insert sheet HLQ at begining of the newly created workBook $new_wb
 		$new_wb = $Excel.workbooks.open($newExcelFile) # open target
 		$sheet_HLQ.Copy($new_wb.Worksheets($Worksheet.Name))
 		$Excel.ActiveWorkbook.Save() 
-        $Excel.ActiveWorkbook.Close
+        $Excel.ActiveWorkbook.Close()
     }
 }
 
 # Closing all 
-$Workbook.Close() 
+$Workbook.Close()
 $Excel.Quit() 
 [System.Runtime.Interopservices.Marshal]::ReleaseComObject($Excel)
 Stop-Process -Name EXCEL
